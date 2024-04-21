5513b7219a5acb44f450aeb33a1b2d062459c958
Ross Cournoyer <rcoupvc@yahoo.com>
2024-04-21 08:48:40 -0700

    colors

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="flex items-center h-16 md:h-20">
-    <h1 class="h-1/2 ml-4 title-font text-4xl">Eat this</h1>
+    <h1 class="h-1/2 ml-6 title-font text-4xl primary-red">Eat this</h1>
   </header>
 </template>
 <script>
